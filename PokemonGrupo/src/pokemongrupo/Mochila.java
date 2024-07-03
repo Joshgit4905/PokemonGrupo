d289fd84af1e21a5706a7b8b67a3b7ef4fa5d5a7
ChinitoPacas31 <santosfutboleros@gmail.com>
2024-07-03 11:17:52 -0600

    Joel Rocha - mochila
    borrar comentarios y agregar un sout con hola mundo

--- PokemonGrupo/src/pokemongrupo/Mochila.java
+++ PokemonGrupo/src/pokemongrupo/Mochila.java
@@ -1,13 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pokemongrupo;
 
-/**
- *
- * @author jrc49
- */
+
 public class Mochila {
+    public static void hola(String[]args){
+    
+        System.out.println("Hola mundo");
+    
+    }
     
 }
