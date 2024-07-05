89e7ddb5250121065d0095a6e0806bc09a422a62
Joshgit4905 <Jrc4905@gmail.com>
2024-07-05 10:46:10 -0600

    Joshua Rodriguez Carrera - mochila
    se hizo el arreglo tridimensional para seleccionar los arreglos bidimensionales que contienen los datos de items y pokemones

--- PokemonGrupo/src/pokemongrupo/ElementosMochila.java
+++ PokemonGrupo/src/pokemongrupo/ElementosMochila.java
@@ -1,7 +1,10 @@
 
 package pokemongrupo;
+
+import java.util.Scanner;
+
 public class ElementosMochila {
-    //private String[][][] mochila;
+    private String[][][] mochila;
     private String[][]  pokebolas;
     private String[][] items;
 
@@ -20,6 +23,61 @@ public class ElementosMochila {
     public void setItems(String[][] items) {
         this.items = items;
     }
+    
+    public String[][][] getmochila() {
+        return mochila;
+    }
+    
+    public void setmochila(String[][][] mochila) {
+        this.mochila = mochila;
+    }
+    public void printMochila (){
+        System.out.println("-------------------------------------");
+        for (int i = 0; i < mochila.length; i++) {
+            System.out.println("");
+            System.out.println("Sección "+(i+1));
+            String[][] stringses = mochila[i];
+            for (int j = 0; j < stringses.length; j++) {
+                String[] stringse = stringses[j];
+                System.out.println("Elemento "+(j+1));
+                for (int k = 0; k < stringse.length; k++) {
+                    System.out.print(" --- "+ stringse[k]); 
+                }
+                System.out.println("");
+            }
+            
+        }
+    }
+    public String[] menu(){
+        Scanner s = new Scanner(System.in);
+        String[] seleccion;
+        int espacio = 0, elemento=0;
+        System.out.println("Selecciona espacio de la mochila \n 1 pokemon \n 2 items");
+        espacio = s.nextInt();
+        if(espacio == 1){
+            System.out.println("Selecciona el pokemon fav");
+            elemento =  s.nextInt();
+            seleccion=  mochila[espacio-1][elemento-1];
+            return seleccion;
+        }else {if (espacio == 2){
+            System.out.println("Selecciona el item fav");
+            elemento =  s.nextInt();
+            seleccion=  mochila[espacio-1][elemento-1];
+            return seleccion;
+        }else{
+                System.out.println("No seleccionaste un espacio correcto");
+               seleccion= new String[1];
+                seleccion[0] ="null data"; 
+                return seleccion;
+                }
+            }
+        }
+    public void printSeleccion(){
+        String[] print = menu();
+        for (int i = 0; i < print.length; i++) {
+            System.out.print(" ---- "+print[i]);  
+        }
+    }
     public void getPokemones(){
         System.out.println("-------------------------------------");
         for (int i = 0; i < pokebolas.length; i++) {
@@ -28,13 +86,11 @@ public class ElementosMochila {
             for (int j = 0; j < pokebola.length; j++) {
                 String dato = pokebola[j];
                 System.out.print(dato+" | ");
-                
             }
             System.out.println("");
             System.out.println("-------------------------------------");
         }
     }
-    
     public void getItem(){
         System.out.println("-------------------------------------");
         for (int i = 0; i < items.length; i++) {
@@ -43,11 +99,9 @@ public class ElementosMochila {
             for (int j = 0; j < item.length; j++) {
                 String dato = item[j];
                 System.out.print(dato+" | ");
-                
             }
             System.out.println("");
             System.out.println("-------------------------------------");
         }
-    }
-    
+    }   
 }
--- PokemonGrupo/src/pokemongrupo/Mochila.java
+++ PokemonGrupo/src/pokemongrupo/Mochila.java
@@ -1,6 +1,6 @@
 
 package pokemongrupo;
-
+import java.util.Scanner;
 import javax.swing.JFrame;
 
 
@@ -10,6 +10,7 @@ public class Mochila{
     public static void main(String[] args) {
         
         ElementosMochila em = new ElementosMochila();
+        
         String[][] pokebolas ={{"01","Bulbasur","Hierba","10","8","55"},
                                {"04","sq","agua","10","8","55"},
                                {"07","char","fuego","10","8","55"}};
@@ -17,16 +18,18 @@ public class Mochila{
         String[][] items = {{"01","baya aranja","15","cant"},
                             {"02","baya citrus","25","cant"},
                             {"03","baya poke","30","cant"}};
+        String[][] bici = {{"Sport","28\""},
+                           {"Normal","21\""}};
         
-                      
-        
+        String mochila[][][] = {pokebolas,items,bici};
         em.setPokebolas(pokebolas);
-        em.getPokemones();
+        //em.getPokemones();
         em.setItems(items);
-        em.getItem();
-        System.out.println("Hola mundo");
-        System.out.println("ariel es gay");
-    System.out.println("y tambien puto");
+        //em.getItem();
+        em.setmochila(mochila);
+        em.printMochila();
+        em.printSeleccion();
+
     }
     
 }
