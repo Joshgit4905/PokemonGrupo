73e8aac4f1884d18676ddacf97016ba4cbf1e083
Alt-Camus <temoltziriverajosuericardo@gmail.com>
2024-07-29 08:39:58 -0600

    Ricardo Temoltzi - Tipo
    Resto del código Tipo.

--- PokemonGrupo/src/pokemongrupo/tipo.java
+++ PokemonGrupo/src/pokemongrupo/tipo.java
@@ -13,3 +13,53 @@ public class tipo {
     
 }
 public static final String PLANTA = "Planta";
+    public static final String FUEGO = "Fuego";
+    public static final String ELECTRICO = "Electrico";
+    public static final String AGUA = "Agua";
+
+    private String tipo;
+
+    public Tipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public double calcularMultiplicador(Tipo oponente) {
+        switch (this.tipo) {
+            case AGUA:
+                if (oponente.getTipo().equals(FUEGO)) {
+                    return 2.0; // Agua es fuerte contra fuego
+                } else if (oponente.getTipo().equals(PLANTA)) {
+                    return 0.5; // Agua es débil contra planta
+                } else if (oponente.getTipo().equals(ELECTRICO)) {
+                    return 0.5; // Agua es débil contra eléctrico
+                }
+                break;
+            case FUEGO:
+                if (oponente.getTipo().equals(AGUA)) {
+                    return 0.5; // Fuego es débil contra agua
+                } else if (oponente.getTipo().equals(PLANTA)) {
+                    return 2.0; // Fuego es fuerte contra planta
+                }
+                break;
+            case PLANTA:
+                if (oponente.getTipo().equals(AGUA)) {
+                    return 2.0; // Planta es fuerte contra agua
+                } else if (oponente.getTipo().equals(FUEGO)) {
+                    return 0.5; // Planta es débil contra fuego
+                }
+                break;
+            case ELECTRICO:
+                if (oponente.getTipo().equals(AGUA)) {
+                    return 2.0; // Eléctrico es fuerte contra agua
+                }
+                break;
+            default:
+                return 1.0; // Pokemon normal sin potenciador
+        }
+        return 1.0; // Pokemon normal sin potenciador
+    }
+}
